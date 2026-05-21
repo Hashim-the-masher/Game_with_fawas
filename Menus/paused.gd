8e2af08207734a57527ca1e12b04738ef30b0ae9
hashem <halfai9234@gmail.com>
2026-05-21 22:03:25 +0300

    Pause that cannot be unpaused

---
+++ Menus/paused.gd
@@ -0,0 +1,74 @@
+extends Control
+
+
+@onready var resume: Label = $HBoxContainer/VBoxContainer/Resume
+@onready var options: Label = $HBoxContainer/VBoxContainer/Options
+@onready var you_did_it: TextureRect = $you_did_it
+var savepath = "user://savedata.json"
+var savedata:Dictionary
+var option_selected:int = 0
+const screen = [1024,512]
+const UI = preload("uid://b5pijvb5s1ujn")
+const UI_SELECTED = preload("uid://dvpvf7wdsrfxi")
+
+func _ready() -> void:
+	savedata = load_json_file()
+	option_selected = savedata["setting_no"]["title"]
+	match savedata["w/l"][0]:
+		1.0:
+			you_did_it.show()
+		0.0:
+			you_did_it.hide()
+	match option_selected:
+		1:
+			resume.label_settings = UI
+			options.label_settings = UI_SELECTED
+		0:
+			resume.label_settings = UI_SELECTED
+			options.label_settings = UI
+	DisplayServer.window_set_size(Vector2i(screen[0]*savedata["settings"]["visuals"][0],screen[1]*savedata["settings"]["visuals"][0]))
+	get_window().content_scale_factor = savedata["settings"]["visuals"][0]
+	match savedata["settings"]["visuals"][1]:
+		1.0:
+			get_window().content_scale_mode = Window.CONTENT_SCALE_MODE_VIEWPORT
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+			print("window fullsckeen")	
+		0.0:
+			get_window().content_scale_mode = Window.CONTENT_SCALE_MODE_DISABLED
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			print("window windowed")	
+
+func load_json_file():
+	var file = FileAccess.open(savepath, FileAccess.READ)
+	var json = file.get_as_text()
+	var jsonobject = JSON.new()
+	jsonobject.parse(json)
+	print("Loaded:"+str(jsonobject.data)+"from file")
+	return jsonobject.data
+
+func save_to_json_file():
+	var file = FileAccess.open(savepath, FileAccess.ModeFlags.WRITE)
+	var json_text = JSON.stringify(savedata)
+	print("written:"+json_text+"to file")
+	file.store_string(json_text)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_down"):
+		option_selected =1
+		resume.label_settings = UI
+		options.label_settings = UI_SELECTED
+		resume.text = "Resume"
+	if event.is_action_pressed("ui_up"):
+		option_selected = 0
+		resume.label_settings = UI_SELECTED
+		options.label_settings = UI
+	if event.is_action_pressed("ui_accept"):
+		match option_selected:
+			1:
+				savedata["setting_no"]["title"] = option_selected
+				save_to_json_file()
+				get_tree().change_scene_to_file("res://Menus/settings.tscn")
+			0:
+				savedata["setting_no"]["title"] = option_selected
+				save_to_json_file()
+				#exit
