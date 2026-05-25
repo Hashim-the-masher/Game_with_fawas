f028dc7debe36725b89044736b2bdfb18f8016c9
Hashem <halfai9234@gmail.com>
2026-05-25 16:23:32 +0300

    optimizing pause code to make implimenting the options easier.

--- Menus/paused_settings.gd
+++ Menus/paused_settings.gd
@@ -1,34 +1,37 @@
 extends Control
 
 @onready var menu_pause: Control = $".."
-@onready var titles = [$main/Titles,$sound/SliderSettings/Titles,$visual/SliderSettings/Titles]
-@onready var values = [null,$"sound/SliderSettings/Display value",$"visual/SliderSettings/Display value"]
-var setting_no:int = 3
+@onready var titles = [[$main/Titles,$sound/SliderSettings/Titles,$visual/SliderSettings/Titles],[],[]]
+@onready var values = [[null,$"sound/SliderSettings/Display value",$"visual/SliderSettings/Display value"]]
+var setting_no = [3,0,0]
 const UI_SETTINGS = preload("uid://b4ckevhw0xmal")
 const UI_SETTINGS_SELECTED = preload("uid://bu6x8ru5xi2kt")
 var back_confirmation_flag = false
+@onready var asettings = get_children(false)
+var current_setting = 0
+
 func _input(event: InputEvent) -> void:
 	if menu_pause.settings == false:
-		hide()
+		asettings[current_setting].hide()
 		return
-	show()
+	asettings[current_setting].show()
 	if event.is_action_pressed("ui_up"):
-		setting_no -= 1
-		setting_no = clampi(setting_no,0,3)
+		setting_no[current_setting] -= 1
+		setting_no[current_setting] = clampi(setting_no[current_setting],0,3)
 		back_confirmation_flag = false
-		titles[0].get_child(3).text = "Back"
+		titles[current_setting][0].get_child(3).text = "Back"
 	if event.is_action_pressed("ui_down"):
-		setting_no += 1
-		setting_no = clampi(setting_no,0,3)
+		setting_no[current_setting] += 1
+		setting_no[current_setting] = clampi(setting_no[current_setting],0,3)
 	if back_confirmation_flag == false:
 		for numbers in 4:
-			if numbers == setting_no:
-				titles[0].get_child(numbers).label_settings = UI_SETTINGS_SELECTED
+			if numbers == setting_no[current_setting]:
+				titles[current_setting][0].get_child(numbers).label_settings = UI_SETTINGS_SELECTED
 			else:
-				titles[0].get_child(numbers).label_settings = UI_SETTINGS
+				titles[current_setting][0].get_child(numbers).label_settings = UI_SETTINGS
 	if event.is_action_pressed("ui_accept"):
 		print(setting_no)
-		match setting_no:
+		match setting_no[current_setting]:
 			0:
 				pass
 			1:
@@ -37,4 +40,5 @@ func _input(event: InputEvent) -> void:
 				pass
 			3:
 				menu_pause.ignore = true
+				asettings[current_setting].hide()
 				menu_pause.settings = false
